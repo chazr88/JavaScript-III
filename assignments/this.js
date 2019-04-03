956d0dccd744989a592d285e87f888e4725043db
chazr88 <chazr_88@hotmail.com>
2019-04-02 20:16:43 -0400

    First push

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,12 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Global Biding. In this instance the 'this' will refer to whatever window you are using. 
+* 2. Implicit Binding. In this instance the 'this' is implied. 
+    When calling a function with a . 'this' is whatever is left of the dot. 
+* 3. New Binding. When a constructor is used, the 'this' is the NEW object. 
+* 4. Explicit Binding. This is when you use a build in JS function like call() or apply() that is 
+    specifically created to tell 'this' what to be. 
 *
 * write out a code example of each explanation above
 */
