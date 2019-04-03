c7534b557620c6578880e59a97c19845d0d29081
chazr88 <chazr_88@hotmail.com>
2019-04-02 21:02:54 -0400

    finished 'this' section

--- assignments/this.js
+++ assignments/this.js
@@ -14,15 +14,38 @@
 // Principle 1
 
 // code example for Window Binding
+console.log(this);
 
 // Principle 2
 
 // code example for Implicit Binding
+const person = {
+    name: 'Chaz',
+    age: 30,
+    occupation: 'programmer',
+    introduce: function(school) {
+        return `My name is ${this.name} I am ${this.age} years old, and I am a ${this.occupation}`;
+    }
+}
+
+console.log(person.introduce());
 
 // Principle 3
 
 // code example for New Binding
+function Programmer(guy) {
+    this.guy = guy,
+    this.language = 'JavaScript';
+    this.program = function () {
+        console.log(`Im ${this.guy} and I am currently writing in ${this.language}`);
+    }  
+}
+
+const Chaz = new Programmer('Chaz');
+const Naruto = new Programmer('Naruto');
+Chaz.program();
 
 // Principle 4
 
 // code example for Explicit Binding
+Chaz.program.call(Naruto);
